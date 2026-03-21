02e410f7dd240183fa5515dbed8cb5e04eacc351
imstuee76 <imstuee76@gmail.com>
2026-03-21 14:18:48 +1000

    Refine ESP config page workflow

--- controller-app/lib/app_version.dart
+++ controller-app/lib/app_version.dart
@@ -1,3 +1,3 @@
-const String controllerVersion = "0.1.27";
-const int controllerBuild = 28;
-const String controllerDisplayVersion = "0.1.27+28";
+const String controllerVersion = "0.1.28";
+const int controllerBuild = 29;
+const String controllerDisplayVersion = "0.1.28+29";
